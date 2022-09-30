142cba8ade374d141b3457e72c3c6cf700b27fa4
Melat Ermiyas <91548860+elumersen@users.noreply.github.com>
2022-09-30 12:10:25 -0700

    Update 1365. How Many Numbers Are Smaller Than the Current Number.py

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -2,12 +2,12 @@ class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         mySortedList = sorted(nums)
         result = []
-        print(mySortedList)
+#         print(mySortedList)
         my_dct= {}
         for i in range(len(mySortedList)):
                 if mySortedList[i] not in my_dct:
                     my_dct[mySortedList[i]] = i 
-        print(my_dct)
+#         print(my_dct)
         for i in nums:
                 result.append(my_dct[i])
         return result
