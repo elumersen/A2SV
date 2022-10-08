a34e1f3a89442e0fdb1f705498bbe4b4b36d3c76
Melat Ermiyas <91548860+elumersen@users.noreply.github.com>
2022-10-08 11:05:39 -0700

    Update 739. Daily Temperatures.py

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -1,10 +1,15 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        warmer_days = [0] * len(temperatures)
-        stack=[]
-        for i , temp in enumerate(temperatures):
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        warmer_days = [0] * n
+        stack = []
+        for i, temp in enumerate(temperatures):
             while len(stack)!=0 and temp > stack[-1][0]:
                 stackTemp, stackIndex = stack.pop()
-                warmer_days[stackIndex] = i-stackIndex
+                warmer_days[stackIndex] = i - stackIndex
             stack.append([temp, i])
         return warmer_days
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
